78d1a2fa57332f476b2514778558b156c8614702
unknown <marcosnapolitano@hotmail.com.ar>
2023-05-30 06:35:16 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,113 @@
+export const en = {
+    introduction1 : `Marcos Napolitano<br>Web Developer<br>based in Spain.`,
+    introduction2 : `I am one of the most dedicated people you will ever work with. 
+                    <br>
+                    I strive to produce the best of the best, always aiming for detailed perfection and 
+                    constantly pushing myself to achieve better-quality solitions for you.
+                    Whatever your dream project is, let me be the one who helps you <i>nail</i> it!`,
+    introduction3 : `Techs I use:`,
+
+
+    about1 : "About me",
+    about2 : "Web Developer from Argentina now based in Cádiz, Spain. <i id='point' class='fa-solid fa-location-pin'></i>",
+    about3 : `I am a super driven person when it comes to delivering great products. 
+              Offering high quality work is something that I am really passionate about since 
+              I am a strong believer that your craft should sell itself when seen. 
+              That is my reason to push myself every day into accomplishing new things and reaching into new territories.`,
+
+
+    portfolio1  : "These are some of my projects!",
+    portfolio2  : "Api's Center",
+    portfolio3  : `This is a compilation of all my api's work. 
+                   I decided to put them all together and give them an actual UI so anyone can interact with them.
+                   Some of them are connected to MongoDb databases.`,
+    portfolio4  : "Tech Stack:",
+    portfolio5  : `Source Code:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio6  : `Live Demo:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio7  : "Django eCommerce",
+    portfolio8  : `A bidding eBay style website made with Django and Python as part of Harvard's CS50W course.
+                   Data is stored in a local SQL database and managed entirely with Django's models. `,
+    portfolio9  : `Source Code:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio10 : `Live Demo:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio11 : "React Calculator",
+    portfolio12 : `A calculator made entirely in React. 
+                   This is a great exercise for taking your coding skills to the next level.
+                   I found it really fun to do!`,
+    portfolio13  : `Source Code:
+                   <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio14 : `Live Demo:
+                   <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio15 : "Random Quote Generator",
+    portfolio16 : `One of my first React Apps, it takes a random quote from a list and makes a nice background transition. 
+                   Simple yet stylish.`,
+    portfolio17 : `Source Code:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio18 : `Live Demo:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+
+    contact1 : "Let's Contact!",
+    contact2 : "You can find me anytime in any of these places:",
+    contact3 : "Or email me at:"
+}
+
+export const esp = {
+
+    introduction1 : `Marcos Napolitano<br>Web Developer<br>residiendo en España.`,
+    introduction2 : `Soy una de las personas más dedicadas con las que podrás trabajar. 
+                     Me esfuerzo día a día para producir lo mejor, siempre aspirando 
+                     a un gran nivel de detalle y constantemente empujandome a lograr 
+                     mejores resultados. Sea cual fuere su proyecto soñado, permítame ser el que lo ayude a <i>alcanzarlo</i>.`,
+    introduction3 : `Tecnologías que utilizo:`,
+
+
+    about1 : "Acerca de",
+    about2 : "Web Developer de Argentina ahora viviendo en Cádiz, España. <i id='point' class='fa-solid fa-location-pin'></i>",
+    about3 : `Soy una persona de gran iniciativa cuando se trata de crear grandes productos. 
+              Soy un apasionado en ofrecer trabajos de alta calidad ya que creo fuertemente en
+              que tu creación debe venderse automáticamente cuando alguien la mira. 
+              Esa es mi razón para empujarme a mí mismo todos los días, lograr nuevas cosas y alcanzar nuevos territorios.`,
+
+
+    portfolio1  : "Estos son algunos de mis Proyectos!",
+    portfolio2  : "Centro de Api's",
+    portfolio3  : `Esta es una recopilación de todo mi trabajo de Api's. 
+                   Decidí unirlas todas y crearles una UI para que todos puedan interactuar con ellas.
+                   Algunas están conectadas a bases de datos MongoDB.`,
+    portfolio4  : "Tech Stack:",
+    portfolio5  : `Código Fuente:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio6  : `Demo en línea:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio7  : "eCommerce Django",
+    portfolio8  : `Un sitio de subastas estilo eBay hecho con Django y Python como parte del curso CS50W de Harvard.
+                   Los datos son almacenados en una base SQL local y manejados con modelos Django`,
+    portfolio9  : `Código Fuente:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio10 : `Demo en línea:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio11 : "Calculadora React",
+    portfolio12 : `Una calculadora hecha íntegramente en React. 
+                   Este es un gran ejercicio para llevar tus habilidades de programador al próximo nivel.
+                   Realmente me pareció muy divertido hacerla!`,
+    portfolio13  : `Código Fuente:
+                   <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio14 : `Demo en línea:
+                   <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+    portfolio15 : "Generador de Citas Aleatorias",
+    portfolio16 : `Una de mis primeras Apps en React, toma una cita de una lista y además realiza una transición suave con el fondo. 
+                   Simple pero elegante`,
+    portfolio17 : `Source Code:
+                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
+    portfolio18 : `Live Demo:
+                    <a><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+
+    contact1 : "Contactemos!",
+    contact2 : "Puedes encontrarme en cualquiera de estos lugares:",
+    contact3 : "O escribirme un mail a:"
+
+}
+
---
+++ script.js
@@ -0,0 +1,55 @@
+import {esp, en} from "./db.js"
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el))
+
+let translated = false
+
+function translate(){
+
+    const lang = translated ? en : esp
+    const elements = document.querySelectorAll(".appear")
+
+    //make the elements disappear
+    for(let i=0;i<elements.length;i++){
+        elements[i].className="disappear"
+    }
+
+    setTimeout(()=>{
+
+        //actual translation
+        for(let i in lang){
+            if(i=="portfolio4") continue
+            document.getElementById(i).innerHTML = lang[i]
+        }
+
+        //show them again
+        for(let i=0;i<elements.length;i++){
+            elements[i].className="appear"
+        }
+
+    },1000)
+
+    
+
+    return translated = !translated
+     
+}
+
+document.getElementById("target").onclick=function(){
+    translate();
+}
+
+
+
+
