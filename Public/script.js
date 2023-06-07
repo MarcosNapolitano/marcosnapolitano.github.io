e519dea046c9a2ec7ddcd129d0d67f4bc0bc3db2
unknown <marcosnapolitano@hotmail.com.ar>
2023-06-07 08:58:47 -0300

    feature: webpage now loads users prefered language

--- Public/script.js
+++ Public/script.js
@@ -13,7 +13,10 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=>observer.observe(el))
 
+const userLang = navigator.language || navigator.userLanguage; 
 let translated = false
+let override = false
+
 
 function translate(){
 
@@ -50,9 +53,23 @@ function translate(){
      
 }
 
+//page loads in spanish if the browser's language is in esp, otherwise loads in eng
+if(userLang[0]+userLang[1]==="es" && !override){
+
+    for(let i in esp){
+        if(i=="portfolio4") continue
+        document.getElementById(i).innerHTML = esp[i]
+
+    }
+
+    document.getElementById("target").innerHTML = "English"
+    override = true
+}
+
 document.getElementById("target").onclick=function(){
     translate();
 }
+
 
 
 
