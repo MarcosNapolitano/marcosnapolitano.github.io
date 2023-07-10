c67b32f2472e9a1fa56104a9b8b6e54f8de26db3
unknown <marcosnapolitano@hotmail.com.ar>
2023-07-10 15:12:03 -0300

    feature: added auction site

--- Public/db.js
+++ Public/db.js
@@ -1,112 +1,105 @@
 export const en = {
-    introduction1 : `Marcos Napolitano<br>Web Developer<br>based in Spain.`,
-    introduction2 : `I am one of the most dedicated people you will ever work with. 
+  introduction1: `Marcos Napolitano<br>Web Developer<br>based in Spain.`,
+  introduction2: `I am one of the most dedicated people you will ever work with. 
                     <br>
                     I strive to produce the best of the best, always aiming for detailed perfection and 
                     constantly pushing myself to achieve better-quality solitions for you.
                     Whatever your dream project is, let me be the one who helps you nail it!`,
-    introduction3 : `Techs I use:`,
+  introduction3: `Techs I use:`,
 
-
-    about1 : "About me",
-    about2 : "Web Developer based in Cádiz, Spain. <i id='point' class='fa-solid fa-location-pin'></i>",
-    about3 : `I'm Marcos, a passionate and creative web developer with 1.5 years of experience in designing and developing stunning, user-friendly websites and apps. I thrive on turning ideas into reality and crafting digital experiences that leave a lasting impression.<br><br>
+  about1: "About me",
+  about2:
+    "Web Developer based in Cádiz, Spain. <i id='point' class='fa-solid fa-location-pin'></i>",
+  about3: `I'm Marcos, a passionate and creative web developer with 1.5 years of experience in designing and developing stunning, user-friendly websites and apps. I thrive on turning ideas into reality and crafting digital experiences that leave a lasting impression.<br><br>
 
     I invite you to explore my portfolio and discover the projects that showcase my skills and expertise. If you have any kind of project in mind or would like to discuss a collaboration, please feel free to get in touch. I am excited about the possibility of bringing your ideas to life and creating a remarkable digital presence together.`,
 
-
-    portfolio1  : "These are some of my projects!",
-    portfolio2  : "Api's Center",
-    portfolio3  : `This is a compilation of all my api's work. 
+  portfolio1: "These are some of my projects!",
+  portfolio2: "Api's Center",
+  portfolio3: `This is a compilation of all my api's work. 
                    I decided to put them all together and give them an actual UI so anyone can interact with them.
                    Some of them are connected to MongoDb databases.`,
-    portfolio4  : "Tech Stack:",
-    portfolio5  : `Source Code:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio6  : `Live Demo:
+  portfolio4: "Tech Stack:",
+  portfolio5: `Source Code:
+                    <a href="https://github.com/MarcosNapolitano/api_center_front"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio6: `Live Demo:
                     <a href="https://marcosnapolitano.github.io/api_center_front/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
-    portfolio7  : "Django Markdown Wiki",
-    portfolio8  : `A mini Markdown wikipedia style app. Entries can be directly edited in Markdown Language and then submitted.
+  portfolio7: "Django Markdown Wiki",
+  portfolio8: `A mini Markdown wikipedia style app. Entries can be directly edited in Markdown Language and then submitted.
                    It has a search feature and links between entries.`,
-    portfolio9  : `Source Code:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio10 : `Live Demo:
+  portfolio9: `Source Code:
+                    <a href="https://github.com/MarcosNapolitano/markdown_wiki"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio10: `Live Demo:
                     <a href="https://marcos-napolitano-markup-wiki.up.railway.app/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
-    portfolio11 : "React Calculator",
-    portfolio12 : `A calculator made entirely in React. 
+  portfolio11: "React Calculator",
+  portfolio12: `A calculator made entirely in React. 
                    This is a great exercise for taking your coding skills to the next level.
                    I found it really fun to do!`,
-    portfolio13  : `Source Code:
-                   <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio14 : `Live Demo:
+  portfolio13: `Source Code:
+                   <a href="https://github.com/MarcosNapolitano/React_Calculator"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio14: `Live Demo:
                    <a href="https://marcosnapolitano.github.io/React_Calculator/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
-    portfolio15 : "Random Quote Generator",
-    portfolio16 : `One of my first React Apps, it takes a random quote from a list and makes a nice background transition. 
-                   Simple yet stylish.`,
-    portfolio17 : `Source Code:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio18 : `Live Demo:
-                    <a href="https://marcosnapolitano.github.io/random_quotes/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+  portfolio15: "Django Auction Site",
+  portfolio16: `An e-bay style auction site with it's own SQL database. Users are able to log in, create auctions and bid or comment on different items.`,
+  portfolio17: `Source Code:
+                    <a href="https://github.com/MarcosNapolitano/auction_site"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio18: `Live Demo:
+                    <a href="https://marcos-napolitano-auctions-site.up.railway.app/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
 
-    contact1 : "Let's Contact!",
-    contact2 : "You can find me on any of these sites:",
-    contact3 : "Or email me at:"
-}
+  contact1: "Let's Contact!",
+  contact2: "You can find me on any of these sites:",
+  contact3: "Or email me at:",
+};
 
 export const esp = {
-
-    introduction1 : `Marcos Napolitano<br>Web Developer<br>en España.`,
-    introduction2 : `Soy una de las personas más dedicadas con las que podrás trabajar. 
+  introduction1: `Marcos Napolitano<br>Web Developer<br>en España.`,
+  introduction2: `Soy una de las personas más dedicadas con las que podrás trabajar. 
                      Me esfuerzo día a día para producir lo mejor, siempre aspirando 
                      a un gran nivel de detalle y constantemente empujandome a lograr 
                      mejores resultados. Sea cual fuere su proyecto soñado, permítame ser el que lo ayude a alcanzarlo.`,
-    introduction3 : `Tecnologías que utilizo:`,
+  introduction3: `Tecnologías que utilizo:`,
 
-
-    about1 : "Acerca de",
-    about2 : "Web Developer en Cádiz, España. <i id='point' class='fa-solid fa-location-pin'></i>",
-    about3 : `Soy Marcos y soy un web developer apasionado y creativo con 1.5 años de experiencia diseñando y creando impactantes, user-friendly websites y apps. Me encargo de convertir ideas en realidades y preparar experiencias digitales que dejen una impresión duradera.<br><br>
+  about1: "Acerca de",
+  about2:
+    "Web Developer en Cádiz, España. <i id='point' class='fa-solid fa-location-pin'></i>",
+  about3: `Soy Marcos y soy un web developer apasionado y creativo con 1.5 años de experiencia diseñando y creando impactantes, user-friendly websites y apps. Me encargo de convertir ideas en realidades y preparar experiencias digitales que dejen una impresión duradera.<br><br>
 
 
     Te invito a explorar mi portfolio y descubrir los proyectos que muestran mis habilidades y conocimientos. Si posees algún tipo de proyecto en mente o si deseara conversar acerca de una colaboración, por favor siéntase libre de ponerse en contacto conmigo. Me gustaría tener la posibilidad de hacer de sus ideas una realidad y de crear una presencia digital notable juntos!`,
 
-
-    portfolio1  : "Estos son algunos de mis Proyectos!",
-    portfolio2  : "Centro de Api's",
-    portfolio3  : `Esta es una recopilación de todo mi trabajo de Api's. 
+  portfolio1: "Estos son algunos de mis Proyectos!",
+  portfolio2: "Centro de Api's",
+  portfolio3: `Esta es una recopilación de todo mi trabajo de Api's. 
                    Decidí unirlas todas y crearles una UI para que todos puedan interactuar con ellas.
                    Algunas están conectadas a bases de datos MongoDB.`,
-    portfolio4  : "Tech Stack:",
-    portfolio5  : `Código Fuente:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio6  : `Demo en línea:
+  portfolio4: "Tech Stack:",
+  portfolio5: `Código Fuente:
+                    <a href="https://github.com/MarcosNapolitano/api_center_front"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio6: `Demo en línea:
                     <a href="https://marcosnapolitano.github.io/api_center_front/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
-    portfolio7  : "Django Markdown Wiki",
-    portfolio8  : `Una aplicación similar a wikipedia con entradas en Markdown. Dichas entradas pueden ser modificadas al momento utilizando idioma Markdown.
+  portfolio7: "Django Markdown Wiki",
+  portfolio8: `Una aplicación similar a wikipedia con entradas en Markdown. Dichas entradas pueden ser modificadas al momento utilizando idioma Markdown.
                     Posee una función de búsqueda y varios links entre entradas.`,
-    portfolio9  : `Código Fuente:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio10 : `Demo en línea:
+  portfolio9: `Código Fuente:
+                    <a href="https://github.com/MarcosNapolitano/markdown_wiki"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio10: `Demo en línea:
                     <a href="https://marcos-napolitano-markup-wiki.up.railway.app/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
-    portfolio11 : "Calculadora React",
-    portfolio12 : `Una calculadora hecha íntegramente en React. 
+  portfolio11: "Calculadora React",
+  portfolio12: `Una calculadora hecha íntegramente en React. 
                    Este es un gran ejercicio para llevar tus habilidades de programador al próximo nivel.
                    Realmente me pareció muy divertido hacerla!`,
-    portfolio13  : `Código Fuente:
-                   <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio14 : `Demo en línea:
-                   <a href="https://marcosnapolitano.github.io/React_Calculator/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`
-                   ,
-    portfolio15 : "Generador de Citas Aleatorias",
-    portfolio16 : `Una de mis primeras Apps en React, toma una cita de una lista y además realiza una transición suave con el fondo. 
-                   Simple pero elegante`,
-    portfolio17 : `Source Code:
-                    <a href="https://github.com/MarcosNapolitano"><i class="fa-brands fa-github" id="github"></i></a>`,
-    portfolio18 : `Live Demo:
-                    <a href="https://marcosnapolitano.github.io/random_quotes/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+  portfolio13: `Código Fuente:
+                   <a href="https://github.com/MarcosNapolitano/React_Calculator"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio14: `Demo en línea:
+                   <a href="https://marcosnapolitano.github.io/React_Calculator/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
+  portfolio15: "Sitio de Subastas Django",
+  portfolio16: `Un sitio similar a e-bay con su propia base de datos SQL. Los usuarios pueden logearse, crear nuevos items, subastar y comentar en ellos.`,
+  portfolio17: `Source Code:
+                    <a href="https://github.com/MarcosNapolitano/auction_site"><i class="fa-brands fa-github" id="github"></i></a>`,
+  portfolio18: `Live Demo:
+                    <a href="https://marcos-napolitano-auctions-site.up.railway.app/"><i class="fa-solid fa-arrow-up-right-from-square" id="github"></i></a>`,
 
-    contact1 : "Contactemos!",
-    contact2 : "Puedes encontrarme en cualquiera de estos lugares:",
-    contact3 : "O escribirme un mail a:"
-
-}
+  contact1: "Contactemos!",
+  contact2: "Puedes encontrarme en cualquiera de estos lugares:",
+  contact3: "O escribirme un mail a:",
+};
